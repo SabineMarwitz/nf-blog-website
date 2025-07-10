baa67db1d38bfd815f2465d76d2d452c22e4e2e3
SabineMarwitz <158596766+SabineMarwitz@users.noreply.github.com>
2025-07-10 18:37:57 +0200

    first implementation using templates and blocks

---
+++ src/app.ts
@@ -0,0 +1,56 @@
+import "dotenv/config";
+
+import express from "express";
+import type { Request, Response } from "express";
+import nunjucks from "nunjucks";
+import blogData from "./data/blog.json";
+import cors from "cors";
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+
+app.use(express.static("public"));
+
+nunjucks.configure("src/templates", {
+  autoescape: true,
+  express: app,
+});
+
+app.get("/", (req: Request, res: Response) => {
+  res.render("index.html", {
+    title: "Home",
+    blogData,
+  });
+});
+
+app.get("/about", (req: Request, res: Response) => {
+  res.render("about.html", {
+    title: "About",
+  });
+});
+
+app.get("/post", (req: Request, res: Response) => {
+  res.render("post.html", {
+    title: "Sample Post",
+  });
+});
+
+app.get("/contact", (req: Request, res: Response) => {
+  res.render("contact.html", {
+    title: "Contact",
+  });
+});
+
+//app.get("/test", (req: Request, res: Response) => {
+//  console.log(blogData);
+//  res.render("_test.html", {
+//    title: "Blogs",
+// blogData,
+//});
+//});
+
+// start the web server
+app.listen(port, () => {
+  console.log(`Example app listening on port${port}`);
+});
