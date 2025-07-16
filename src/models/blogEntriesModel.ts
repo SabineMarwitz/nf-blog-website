be857db455aa284374c7da43609980481c968d5c
SabineMarwitz <158596766+SabineMarwitz@users.noreply.github.com>
2025-07-16 19:36:33 +0200

    added basic functions for add and remove a post

--- src/models/blogEntriesModel.ts
+++ src/models/blogEntriesModel.ts
@@ -1,9 +1,10 @@
 import { readFile, writeFile } from "node:fs/promises";
 import * as path from "node:path";
-import type { BlogEntries } from "../types/models";
+import type { BlogEntries, BlogEntry } from "../types/models";
 
 const FILE_PATH = path.join(__dirname, "..", "data", "blog.json");
-const TEST_FILE_PATH = path.join(__dirname, "..", "data", "test.json");
+// ** Test
+//const TEST_FILE_PATH = path.join(__dirname, "..", "data", "test.json");
 
 export async function getAllBlogEntries(): Promise<BlogEntries> {
   try {
@@ -19,19 +20,36 @@ export async function getAllBlogEntries(): Promise<BlogEntries> {
   }
 }
 
-// todo: add models to add, update, delete data
-
 // add blog
-export async function addBlogEntry() {
+export async function addBlogEntry(testEntry: BlogEntry) {
   try {
-    const blogEntries = await readFile(FILE_PATH, { encoding: "utf-8" });
-    // todo add a blog before write back
-    // ...
-    await writeFile(TEST_FILE_PATH, blogEntries);
-    // Test
-    const data = await readFile(TEST_FILE_PATH, { encoding: "utf-8" });
-    console.log(data.length);
+    const blogEntries = await getAllBlogEntries();
+    blogEntries.push(testEntry);
+    await writeFile(FILE_PATH, JSON.stringify(blogEntries));
+    // ** Test
+    // const data = await readFile(FILE_PATH, { encoding: "utf-8" });
+    // console.log("Test: new data length ", data.length);
+    // **
   } catch (err) {
     console.error("ERROR in addBlogEntry: ", err);
   }
 }
+
+// delete blog
+export async function deleteBlogEntry(index: number) {
+  try {
+    const blogEntries = await getAllBlogEntries();
+    if (blogEntries.length > 0) {
+      blogEntries.splice(index, 1);
+    }
+    await writeFile(FILE_PATH, JSON.stringify(blogEntries));
+    // ** Test
+    // const data = await readFile(FILE_PATH, { encoding: "utf-8" });
+    // console.log("Test: new data length ", data.length);
+    // **
+  } catch (err) {
+    console.error("ERROR in deleteBlogEntry: ", err);
+  }
+}
+
+// todo: update a blog
