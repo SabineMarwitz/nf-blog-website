b98a0ff8c54529706510dfb9556aaeef5b3ad6e4
SabineMarwitz <158596766+SabineMarwitz@users.noreply.github.com>
2025-07-16 19:38:32 +0200

    added test code for add and remove a blog entry

--- src/controllers/admin/blogController.ts
+++ src/controllers/admin/blogController.ts
@@ -1,12 +1,30 @@
 import type { Request, Response } from "express";
-import { BlogEntries } from "../../types/models";
-import { getAllBlogEntries, addBlogEntry } from "../../models/blogEntriesModel";
+import { BlogEntries, BlogEntry } from "../../types/models";
+import {
+  getAllBlogEntries,
+  addBlogEntry,
+  deleteBlogEntry,
+} from "../../models/blogEntriesModel";
+
+/* ******************
+ * only for testing *
+ * ******************/
+const testEntry: BlogEntry = {
+  title: "New Blog",
+  image: "sailing.jpg",
+  author: "Paul",
+  createdAt: 1743120000,
+  teaser: "This is a test for adding a block",
+  content: "<p>Test function addBlogEntry </p>",
+};
 
 export const entriesListing = async (req: Request, res: Response) => {
-  await addBlogEntry();
+  // only for testing
+  // await addBlogEntry(testEntry);
+  // await deleteBlogEntry(3);
+
   // admin view should list all blog enttries
   const blogEntries = await getAllBlogEntries();
-  console.log("hdsahakakdka");
   res.render("../views/admin/indexPage.html", {
     title: "Admin",
     blogEntries,
