6e44214ea57249873d24983c3a87368cf3da8ae3
SabineMarwitz <158596766+SabineMarwitz@users.noreply.github.com>
2025-07-16 12:57:12 +0200

    list all blog entries on admin dashboard

--- src/controllers/admin/blogController.ts
+++ src/controllers/admin/blogController.ts
@@ -1,5 +1,13 @@
 import type { Request, Response } from "express";
+import { BlogEntries } from "../../types/models";
+import { getAllBlogEntries } from "../../models/blogEntriesModel";
 
-export const entriesListing = (req: Request, res: Response) => {
-  res.render("../views/admin/indexPage.html", {});
+export const entriesListing = async (req: Request, res: Response) => {
+  // admin view should list all blog enttries
+  const blogEntries = await getAllBlogEntries();
+  console.log(blogEntries.length);
+  res.render("../views/admin/indexPage.html", {
+    title: "Admin",
+    blogEntries,
+  });
 };
