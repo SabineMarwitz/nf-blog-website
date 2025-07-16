a1686f04063d56b1acf30ebeb59b981b48bb8334
SabineMarwitz <158596766+SabineMarwitz@users.noreply.github.com>
2025-07-16 16:24:45 +0200

    to test

--- src/controllers/admin/blogController.ts
+++ src/controllers/admin/blogController.ts
@@ -1,11 +1,12 @@
 import type { Request, Response } from "express";
 import { BlogEntries } from "../../types/models";
-import { getAllBlogEntries } from "../../models/blogEntriesModel";
+import { getAllBlogEntries, addBlogEntry } from "../../models/blogEntriesModel";
 
 export const entriesListing = async (req: Request, res: Response) => {
+  await addBlogEntry();
   // admin view should list all blog enttries
   const blogEntries = await getAllBlogEntries();
-  console.log(blogEntries.length);
+  console.log("hdsahakakdka");
   res.render("../views/admin/indexPage.html", {
     title: "Admin",
     blogEntries,
--- src/models/blogEntriesModel.ts
+++ src/models/blogEntriesModel.ts
@@ -1,8 +1,9 @@
-import { readFile } from "node:fs/promises";
+import { readFile, writeFile } from "node:fs/promises";
 import * as path from "node:path";
 import type { BlogEntries } from "../types/models";
 
 const FILE_PATH = path.join(__dirname, "..", "data", "blog.json");
+const TEST_FILE_PATH = path.join(__dirname, "..", "data", "test.json");
 
 export async function getAllBlogEntries(): Promise<BlogEntries> {
   try {
@@ -19,3 +20,18 @@ export async function getAllBlogEntries(): Promise<BlogEntries> {
 }
 
 // todo: add models to add, update, delete data
+
+// add blog
+export async function addBlogEntry() {
+  try {
+    const blogEntries = await readFile(FILE_PATH, { encoding: "utf-8" });
+    // todo add a blog before write back
+    // ...
+    await writeFile(TEST_FILE_PATH, blogEntries);
+    // Test
+    const data = await readFile(TEST_FILE_PATH, { encoding: "utf-8" });
+    console.log(data.length);
+  } catch (err) {
+    console.error("ERROR in addBlogEntry: ", err);
+  }
+}
